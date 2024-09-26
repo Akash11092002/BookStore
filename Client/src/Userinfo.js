8d19f3f71480bdeabc4fdce7f0dee1d86b91a9da
AKASH MAURYA <114644236+Akash11092002@users.noreply.github.com>
2024-09-26 12:10:06 +0530

    Update Userinfo.js

--- Client/src/Userinfo.js
+++ Client/src/Userinfo.js
@@ -9,7 +9,7 @@ function Userinfo() {
 
   const fetchData = async () => {
     try {
-      const response = await fetch('http://localhost:8091/users');
+      const response = await fetch('https://bookstore-backend-xj98.onrender.com/users');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
