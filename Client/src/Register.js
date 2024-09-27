fe1b5fa945c58349dbf6cc3b36347a65c60bbd09
AKASH MAURYA <114644236+Akash11092002@users.noreply.github.com>
2024-09-27 22:39:40 +0530

    Update Register.js

--- Client/src/Register.js
+++ Client/src/Register.js
@@ -18,7 +18,7 @@ const Register = () => {
   async function handleSubmit(e) {
     e.preventDefault();
     
-      const response = await fetch('https://bookstore-backend-xj98.onrender.com/registrationHandler', { 
+      const response = await fetch('https://bookstore-backend-boit.onrender.com/registrationHandler', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
