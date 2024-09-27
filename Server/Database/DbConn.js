c8133bb75c5d537b761fe571d6b0f0ccd6c7e804
AKASH MAURYA <114644236+Akash11092002@users.noreply.github.com>
2024-09-27 22:13:06 +0530

    Update DbConn.js

--- Server/Database/DbConn.js
+++ Server/Database/DbConn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/mybook', {
+    await mongoose.connect('mongodb+srv://mauryaakash3965:Ak118665@@cluster0.g7er0.mongodb.net/', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
